ee57441186067ca5ae4562335e0889d8d5dd921c
akhil chaudhary <chaudharyakhil337@gmail.com>
2021-09-28 00:45:12 +0530

    commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlkPlc2j9bGD3RDk5pVdOlluJ3kiquZUc",
+    authDomain: "clone-b819a.firebaseapp.com",
+    projectId: "clone-b819a",
+    storageBucket: "clone-b819a.appspot.com",
+    messagingSenderId: "159086974385",
+    appId: "1:159086974385:web:07803a523b2a45da9b9990"
+  };
